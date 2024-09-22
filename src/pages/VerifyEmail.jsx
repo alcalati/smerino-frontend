1a219463cd1e1abcdbd3efaa06a9c79db01dc2a1
alcalati <alcaat@edem.es>
2024-09-22 02:49:08 +0200

    verify-email

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -6,26 +6,30 @@ const VerifyEmail = () => {
   const { token } = useParams();  // Captura el token de la URL
   const [message, setMessage] = useState('');
   const navigate = useNavigate();
+  const [hasVerified, setHasVerified] = useState(false);
 
-  useEffect(() => {
-    const verifyEmail = async () => {
-      try {
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/auth/verify/${token}`);
-        setMessage(response.data.message);
-      } catch (error) {
-        console.error('Error en la solicitud de verificación:', error);
-        if (error.response) {
-          setMessage(`Error: ${error.response.data.message}`); // Mensaje del backend
-        } else {
-          setMessage('Error al verificar el correo.'); // Mensaje genérico
-        }
+useEffect(() => {
+  const verifyEmail = async () => {
+    if (hasVerified) return; // Evitar múltiples verificaciones
+
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/auth/verify/${token}`);
+      setMessage(response.data.message);
+      setHasVerified(true); // Marca como verificado
+    } catch (error) {
+      console.error('Error en la solicitud de verificación:', error);
+      if (error.response) {
+        setMessage(`Error: ${error.response.data.message}`);
+      } else {
+        setMessage('Error al verificar el correo.');
       }
-    };
-
-    if (token) { // Asegúrate de que el token esté presente antes de hacer la llamada
-      verifyEmail();
     }
-  }, [token]);
+  };
+
+  if (token) {
+    verifyEmail();
+  }
+}, [token, hasVerified]); // Agrega hasVerified como dependencia
 
 
   return (
