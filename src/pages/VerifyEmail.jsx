298cde6ce12fa6bbf8d4bb320004b0f7e2b248eb
alcalati <alcaat@edem.es>
2024-09-22 03:16:27 +0200

    verify-email

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -6,35 +6,36 @@ const VerifyEmail = () => {
   const { token } = useParams();  // Captura el token de la URL
   const [message, setMessage] = useState('');
   const navigate = useNavigate();
-  const [hasVerified, setHasVerified] = useState(false);
 
-useEffect(() => {
-  const verifyEmail = async () => {
-    if (hasVerified) return; // Evitar múltiples verificaciones
-
-    try {
-      const response = await axios.get(`${process.env.REACT_APP_API_URL}/auth/verify/${token}`);
-      setMessage(response.data.message);
-      setHasVerified(true); // Marca como verificado
-    } catch (error) {
-      console.error('Error en la solicitud de verificación:', error);
-      if (error.response) {
-        setMessage(`Error: ${error.response.data.message}`);
-      } else {
-        setMessage('Error al verificar el correo.');
+  useEffect(() => {
+    const verifyEmail = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/auth/verify/${token}`);
+        // Almacenar el mensaje de éxito en el estado
+        setMessage(response.data.message);
+        // Redirigir después de un pequeño retraso para mostrar el mensaje
+        setTimeout(() => {
+          navigate('/email-verified');
+        }, 2000); // Espera 2 segundos antes de redirigir
+      } catch (error) {
+        console.error('Error en la solicitud de verificación:', error);
+        if (error.response) {
+          setMessage(`Error: ${error.response.data.message}`); // Mensaje del backend
+        } else {
+          setMessage('Error al verificar el correo.'); // Mensaje genérico
+        }
       }
+    };
+
+    if (token) { // Asegúrate de que el token esté presente antes de hacer la llamada
+      verifyEmail();
     }
-  };
-
-  if (token) {
-    verifyEmail();
-  }
-}, [token, hasVerified]); // Agrega hasVerified como dependencia
-
+  }, [token, navigate]);
 
   return (
     <div className="container">
-      <h2>Verificando Correo...</h2>
+      <h2>Verificación de Correo</h2>
+      <p>{message}</p>
     </div>
   );
 };
